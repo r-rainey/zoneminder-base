529cc3465106be8356e9786c99d0678a91f75cab
Roger Rainey <rrainey@lineagelogitsics.com>
2024-05-24 01:44:43 -0700

    fix nginx.conf

--- rootfs/etc/cont-init.d/60-janus-config.sh
+++ rootfs/etc/cont-init.d/60-janus-config.sh
@@ -3,5 +3,5 @@
 program_name="janus-config"
 
 echo "Configuring janus settings..." | info "[${program_name}] "
-# sed -i "s/FASTCGI_BUFFERS_CONFIGURATION_STRING/${FASTCGI_BUFFERS_CONFIGURATION_STRING}/g" /etc/janus/janus.jcnf
-# sed -i "s/PHP_VERSION_ENVIRONMENT_VARIABLE/${PHP_VERSION}/g" /etc/janus.jcnf
+sed -i "s/FASTCGI_BUFFERS_CONFIGURATION_STRING/${FASTCGI_BUFFERS_CONFIGURATION_STRING}/g" /etc/janus/janus.jcfg
+sed -i "s/PHP_VERSION_ENVIRONMENT_VARIABLE/${PHP_VERSION}/g" /etc/janus.jcfg
